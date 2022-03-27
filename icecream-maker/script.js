b00bc104476d95a30f1cb12bdd4d7fb77afe9754
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-03-27 09:49:16 +0600

    Worked on Script File

--- icecream-maker/script.js
+++ icecream-maker/script.js
@@ -3,3 +3,4 @@ let scoop2 = document.querySelector(".scoop2");
 let scoop3 = document.querySelector(".scoop3");
 
 let btnScoop1 = document.getElementById("btn-scoop1");
+let btnScoop2 = document.getElementById("btn-scoop2");
