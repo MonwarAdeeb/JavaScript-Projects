276866ccf3d32f831111d41f946c3353637fa089
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-03-31 08:16:34 +0600

    Worked on Script File

--- icecream-maker/script.js
+++ icecream-maker/script.js
@@ -7,3 +7,5 @@ let btnScoop2 = document.getElementById("btn-scoop2");
 let btnScoop3 = document.getElementById("btn-scoop3");
 let btnTopping1 = document.getElementById("btn-topping1");
 let btnTopping2 = document.getElementById("btn-topping2");
+
+let colors = ["#f45c96", "#f8bd3d", "#badc58", "#5c0003", "#f58619", "#ebab70"];
