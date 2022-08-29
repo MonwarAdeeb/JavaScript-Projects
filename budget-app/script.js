52f6348a5b1fb80957c6294d396413582f080081
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-08-29 10:00:00 +0600

    Worked on Script File

--- budget-app/script.js
+++ budget-app/script.js
@@ -3,3 +3,5 @@ let userAmount = document.getElementById("user-amount");
 const checkAmountButton = document.getElementById("check-amount");
 const totalAmountButton = document.getElementById("total-amount-button");
 const productTitle = document.getElementById("product-title");
+const errorMessage = document.getElementById("budget-error");
+const productTitleError = document.getElementById("product-title-error");
