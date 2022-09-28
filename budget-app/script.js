afacd36dd4bb225ca26c04f2fe192828dae1cde6
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-09-28 15:19:04 +0600

    Worked on Script File

--- budget-app/script.js
+++ budget-app/script.js
@@ -45,6 +45,7 @@ const modifyElement = (element, edit = false) => {
     let parentAmount = parentDiv.querySelector(".amount").innerText;
     if (edit) {
         let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
 
     }
 };
