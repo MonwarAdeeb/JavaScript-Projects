10732935e184986a0a5824a9712ab6c65efce8e0
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-10-21 12:34:40 +0600

    Worked on Script File

--- budget-app/script.js
+++ budget-app/script.js
@@ -71,6 +71,7 @@ const listCreator = (expenseName, expenseValue) => {
     deleteButton.classList.add("fa-solid", "fa-trash-can", "delete");
     deleteButton.style.fontSize = "1.2em";
     deleteButton.addEventListener("click", () => {
-
+        modifyElement(deleteButton);
     });
+
 };
