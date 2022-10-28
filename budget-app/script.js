ed660afeae65dc86b70e0d87e894fb0dcc7cb3b5
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-10-28 10:00:00 +0600

    Worked on Script File

--- budget-app/script.js
+++ budget-app/script.js
@@ -77,3 +77,11 @@ const listCreator = (expenseName, expenseValue) => {
     sublistContent.appendChild(deleteButton);
     document.getElementById("list").appendChild(sublistContent);
 };
+
+//Function To Add Expenses
+checkAmountButton.addEventListener("click", () => {
+    //empty checks
+    if (!userAmount.value || !productTitle.value) {
+
+    }
+});
